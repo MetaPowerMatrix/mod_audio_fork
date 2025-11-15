871bbf312a473c9c985ce78b2a4adca96deac366
MetaPowerMatrix <lucawilliams0248@gmail.com>
2025-11-15 14:37:18 +0800

    Update build_audio_fork.sh

--- build_audio_fork.sh
+++ build_audio_fork.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Cập nhật danh sách gói
-apt-get update
+# apt-get update
 
 # Cài các thư viện cần thiết
-apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev libwebsockets-dev
+# apt-get -y install libfreeswitch-dev libssl-dev zlib1g-dev libspeexdsp-dev libwebsockets-dev
 
 # Tạo thư mục build và vào đó
 mkdir -p build && cd build
